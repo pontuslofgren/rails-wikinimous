7a78e86f4b501583b56f6d77887512d35f8a899b
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-11-14 16:50:09 +0000

    Users can index, show, create, update, delete. Added Bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,38 @@
 class ArticlesController < ApplicationController
   before_action :set_articles, except: %i[]
-  before_action :set_article, except: %i[index]
+  before_action :set_article, except: %i[index new create]
 
   def index
     # @articles = Article.all
   end
 
-  # def show
-  # end
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
@@ -17,5 +42,9 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
 
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: 'edit'
+  patch 'articles/:id', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy'
+
   get 'articles', to: 'articles#index'
   get 'articles/:id', to: 'articles#show', as: 'article'
+
 end
